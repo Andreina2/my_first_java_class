c4ba68bc25435aa391ce397ab86bb92a60dbc95f
Andreina2 <andreinaarias5@gmail.com>
2024-11-11 12:19:52 +0100

    create Readme.md, .gitignore and class Person

---
+++ src/main/java/dev/andreina/my_first_java_class/Person.java
@@ -0,0 +1,11 @@
+package dev.andreina.my_first_java_class;
+
+public class Person {
+    public String name;
+    public String lastName;
+    public char documentId;
+    public int yearOfBirth;
+    public String countryOfBirth;
+    public String gender;
+
+}
